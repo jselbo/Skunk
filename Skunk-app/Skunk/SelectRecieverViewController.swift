c3aa3bfc2b2b0a6c7a7bee066c3cc0a03590a652
Don Phan <phand@purdue.edu>
2015-09-20 14:05:48 -0400

    [feat]: Created SelectReieverViewController

---
+++ Skunk-app/Skunk/SelectRecieverViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SelectRecieverViewController.swift
+//  Skunk
+//
+//  Created by Don Phan on 9/20/15.
+//  Copyright © 2015 CS408. All rights reserved.
+//
+
+import UIKit
+
+class SelectRecieverViewController: UIViewController {
+    @IBOutlet weak var friendsList: UIPickerView!
+    @IBOutlet weak var sendRequest: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
