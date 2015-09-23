4c88b61f1bc9bd9c8612b8339edb83ed6bd4625e
Anant Goel <goela@purdue.edu>
2015-09-23 14:32:33 -0400

    [feat/fix]: Added Sharer list view for Receiver, fixed initial Receiver view
    The sharer list for the receiever still needs to be implemented as
    table cells.

---
+++ Skunk-app/Skunk/ReceiveFriendsListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ReceiveFriendsListViewController.swift
+//  Skunk
+//
+//  Created by Anant Goel on 9/23/15.
+//  Copyright © 2015 CS408. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ReceiveFriendsListViewController: UIViewController {
+    
+    @IBOutlet weak var friendsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- Skunk-app/Skunk/ReceiveMainViewController.swift
+++ Skunk-app/Skunk/ReceiveMainViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ReceiveMainViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
