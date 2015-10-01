496761a2cbf6c0f8a476c7cba061c75fa4a52b54
Jon Egeland <audiobahn404@gmail.com>
2015-10-01 17:05:09 -0400

    [Feat] User actions should be complete. Create and login both seem to work

--- server/server.rb
+++ server/server.rb
@@ -2,11 +2,43 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'mysql2'
 
+# Rack is being a bit weird with thin, making some random asserts. Let's just
+# ignore those, shall we?
+module Rack
+  class Lint
+    def assert message, &block
+    end
+  end
+end
 
+# Everything we do here is JSON, so default to returning it.
+before do
+  content_type 'application/json'
+end
+
+helpers do
+  # A singular point for encrypting different values across the server
+  def encrypt needle
+    Digest::SHA2.hexdigest(needle)
+  end
+end
 
 # To reduce merge conflicts and such, separate all of the action logic into
 # different files. This may eventually need to be further separated depending
 # on how complex the interactions get.
+
+# #
+# Models
+# #
+
+require './models/session.rb'
+require './models/session_user.rb'
+require './models/user.rb'
+
+
+# #
+# Controllers
+# #
 
 # sessions.rb contains the endpoint for basic management of session objects.
 require './controllers/sessions.rb'
