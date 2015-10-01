cdc3b7c5e7272c03df734ea3acbd47c0b8817133
Jon Egeland <audiobahn404@gmail.com>
2015-10-01 17:05:09 -0400

    [Feat] User actions should be complete. Create and login both seem to work

--- server/controllers/users.rb
+++ server/controllers/users.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 # POST /users/create
 # {
 #   "name": "username",
@@ -6,14 +8,61 @@
 #   ...
 # }
 # -> 200 OK <user_id>
+# -> 201 Created
 # -> 500 Internal Server Error
 #
 # This endpoint is hit whenever a new user registers to use the app. The body
-# of the request contains any information that the user needs to identify
-# themselves in the system (the contents of which is yet to be determined).
+# of the request contains all of the information that the user needs to
+# identify themselves in the system (the contents of which is yet to be
+# determined).
+#
+# On success, returns the unique ID for the user (a SHA-2 Hash value), and
+# a boolean indicating whether or not a new user was created.
+# On error, returns a 500 Internal Server Error with details about what went
+# wrong.
+post '/users/create' do
+  # Filter the parameters from the request JSON
+  user_params = {
+    name: params['name'],
+    email: params['email'],
+    password: params['password']
+  }
+  # See if the User already exists
+  @user = User.find_by_identity user_params
+  if @user
+    # If they do exist, return their ID and the fact that they already exist
+    return { user_id: @user.id, created: false }.to_json
+  else
+    # Otherwise, create a new User object and save it...
+    @user = User.create user_params
+    # ...and return the appropriate information
+    return { user_id: @user.id, created: true }.to_json
+  end
+end
+
+# POST /users/login
+# {
+#   "name": "username",
+#   "password": "userpassword"
+# }
+# -> 200 OK <user_id>
+# -> 404 Not Found
+# -> 500 Internal Server Error
+#
+# This endpoint is hit whenever a registered user loads the app. The body
+# of the request contains any information that the user needs to verify their
+# identity with what is in the system.
 #
 # On success, returns the unique ID for the user (a SHA-2 Hash value).
 # On error, returns a 500 Internal Server Error with details about what went
 # wrong.
-post '/users/create' do
+post '/users/login' do
+  # Encrypt the user's password to match what's in the database
+  params[:password] = encrypt(params[:password])
+  # Try to find the user by their credentials
+  @user = User.find_by_credentials params
+  # If the user does not exist, return a 404
+  halt(404) unless @user
+  # If they do exist, return their ID.
+  { user_id: @user.id }.to_json
 end
