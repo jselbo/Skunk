849c2e8bbd9cfdd8106ba63f2a269ab12d8b0027
Sean McCullough <mccullo0@purdue.edu>
2015-10-24 15:27:34 -0400

    added sessions factory for testing

--- server/spec/factories.rb
+++ server/spec/factories.rb
@@ -5,4 +5,31 @@ FactoryGirl.define do
     phone_number { Faker::PhoneNumber.phone_number }
     password { Faker::Internet.password }
   end
+	
+  factory :session do
+	association :sharer, factory: :user, strategy: :build
+    sharer_id { Faker::user.id }
+    needs_driver { False } #TODO: does this need to be randomized?
+    driver_id nil
+    
+    start_time { DateTime.now  }
+    is_time_based True
+    end_time { Faker::Time.backward() }    
+    
+    destination nil
+    terminated False
+
+    #TODO: define the time threshold in the session object probably
+    last_updated { Faker::Time.between(15.minutes.ago, Time.now }
+    requested_pickup False
+    driver_eta { Faker::Time.forward()  }
+
+    current_location "#{ Faker::Address.latitude}#{Faker::Address.longitude }" 
+    factory :session_with_destination do 
+        is_time_based True
+        destination "#{ Faker::Address.latitude}#{Faker::Address.longitude }" 
+    factory :session_with_driver do
+        association :driver, factory: :user, strategy: :build
+        driver_id driver.id
+    end
 end
