5f4562f052ab6553b8d56f14ca24bdf930f8b5ec
Jon Egeland <audiobahn404@gmail.com>
2015-10-25 15:04:04 -0400

    [Fix] POST /sessions/create works

--- server/models/session.rb
+++ server/models/session.rb
@@ -1,22 +1,20 @@
 require 'active_record'
 
 class Session < ActiveRecord::Base
+  has_many :session_users
 	has_many :receivers, through: :session_users
 	belongs_to :driver, class_name: "User"
 
-	def as_json
-		to_json(include: [:sharer, :driver], except: {sharer: [:password], driver: [:password]})
-	end 
-	
-	#Ensure that a string is a location in iso 6709  format
-	#Returns true on success, fale on failure.
-	def check_location( location )
-		#TODO: Need any type checking on the string?
-
-		if /^[+-][0-9]+\.?[0-9]*[+-][0-9]+\.?[0-9]*$/.match(location)
-			return true
-		else
-			return false
-		end
+	def to_json
+		as_json(include: [:sharer, :driver])
 	end
+
+  class << self
+  	# Ensure that a string is a location in iso 6709  format
+  	# Returns true on success, fale on failure.
+  	def check_location location
+  		# TODO: Need any type checking on the string?
+  		/^[+-][0-9]+\.?[0-9]*,[+-][0-9]+\.?[0-9]*$/.match(location)
+  	end
+  end
 end
