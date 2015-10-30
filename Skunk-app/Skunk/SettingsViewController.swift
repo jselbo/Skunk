977cc3ed436d20a7ca174173600dc646abb026a3
Joshua Selbo <joshuaselbo@gmail.com>
2015-10-29 20:10:01 -0400

    [fix] Fix critical stability problem

--- Skunk-app/Skunk/SettingsViewController.swift
+++ Skunk-app/Skunk/SettingsViewController.swift
@@ -16,6 +16,8 @@ class SettingsViewController: UITableViewController {
     @IBOutlet weak var logOutCell: UITableViewCell!
     @IBOutlet weak var phoneNumber: UITableViewCell!
     
+    var numberPoopyTaps = 0
+    
     override func viewDidLoad() {
         let account = accountManager.registeredAccount!
         var nameText = account.userAccount.fullName
@@ -25,6 +27,9 @@ class SettingsViewController: UITableViewController {
         
         name.detailTextLabel!.text = nameText
         phoneNumber.detailTextLabel!.text = account.userAccount.phoneNumber.formatForUser()
+        
+        let tap = UITapGestureRecognizer(target: self, action: "poopyTap")
+        name.contentView.addGestureRecognizer(tap)
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -36,6 +41,33 @@ class SettingsViewController: UITableViewController {
             break
         default:
             break
+        }
+    }
+    
+    func poopyTap() {
+        numberPoopyTaps++
+        if numberPoopyTaps >= 10 {
+            spawnPoop()
+        }
+    }
+
+    func spawnPoop() {
+        let poopLabel = UILabel()
+        poopLabel.text = "💩"
+        poopLabel.font = UIFont.systemFontOfSize(128.0)
+        self.view.addSubview(poopLabel)
+        
+        poopLabel.sizeToFit()
+        
+        let maxX = UInt32(self.view.frame.size.width - poopLabel.frame.size.width)
+        let randomX = Int(arc4random_uniform(maxX))
+        poopLabel.frame.origin = CGPointMake(CGFloat(randomX), -poopLabel.frame.size.height)
+        
+        UIView.animateWithDuration(2.0, delay: 0.0, options: [.CurveEaseIn], animations: { () -> Void in
+            let height = self.view.frame.size.height
+            poopLabel.frame.origin = CGPointMake(CGFloat(randomX), height)
+        }) { (done) -> Void in
+            poopLabel.removeFromSuperview()
         }
     }
     
